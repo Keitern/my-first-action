a5a8f29d649e997dc673960c962ba7739e855eae
Trace <d.nguyen@live.dk>
2026-03-16 10:14:33 +0100

    Add CI workflow

---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+// src/calculator.js
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Inputs must be numbers');
+    }
+    return a + b;
+}
+module.exports = { add };
---
+++ src/calculator.test.js
@@ -0,0 +1,10 @@
+// src/calculator.test.js
+const { add } = require('./calculator');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('throws error for non-number inputs', () => {
+    expect(() => add('1', 2)).toThrow('Inputs must be numbers');
+});
